1d40f4ca72f5405028e481d43affab2641bacb7a
Joan Andrés <andres6936@live.com>
2020-12-12 09:05:14 -0500

    Write methods

--- App/Chunks.py
+++ App/Chunks.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QByteArray, QObject
+from PyQt5.QtCore import QByteArray, QObject, QIODevice
 
 class Chunk:
     def __init__(self):
@@ -7,5 +7,30 @@ class Chunk:
         self.absPos : int = 0
 
 class Chunks(QObject):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent : QObject = None, device : QIODevice = None):
+        super().__init__(parent)
+        self.device = QIODevice() if device is None else device
+        self.chunks : [Chunk] = []
+        self.position = 0
+        self.size = 0
+
+    def setIODevice(self, device : QIODevice) -> bool:
+        pass
+
+    def data(self, position : int, count : int = -1, highlighted : QByteArray = None) -> QByteArray:
+        pass
+
+    def write(self, device : QIODevice, position : int = 0, count : int = -1) -> bool:
+        pass
+
+    def setDataChanged(self, position : int, dataChanged : bool) -> None:
+        pass
+
+    def dataChanged(self, position : int) -> bool:
+        pass
+
+    def indexOf(self, array : QByteArray, _from : int) -> int:
+        pass
+
+    def lastIndexOf(self, array : QByteArray, _from : int) -> int:
+        pass
