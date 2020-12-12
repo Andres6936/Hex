77942b94538d80c0cafe70b20dcd07d88c9e709b
Joan Andrés <andres6936@live.com>
2020-12-12 00:09:52 -0500

    Implemented Chunks

--- App/Chunks.py
+++ App/Chunks.py
@@ -1,2 +1,11 @@
-class Chunks:
-    pass
+from PyQt5.QtCore import QByteArray, QObject
+
+class Chunk:
+    def __init__(self):
+        self.data = QByteArray()
+        self.dataChanged = QByteArray()
+        self.absPos : int = 0
+
+class Chunks(QObject):
+    def __init__(self):
+        super().__init__()
