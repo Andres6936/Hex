3a6695ce01a4d3bef0a6dbe9d9c42d6bbdab13ca
Joan Andrés <andres6936@live.com>
2020-12-12 22:25:39 -0500

    Write the class UndoStack

--- App/UndoStack.py
+++ App/UndoStack.py
@@ -1,2 +1,21 @@
-class UndoStack:
-    pass
+from PyQt5.QtWidgets import QUndoStack
+
+
+class UndoStack(QUndoStack):
+    def __init__(self):
+        super().__init__()
+
+    def removeAt(self):
+        pass
+
+    def insertChar(self):
+        pass
+
+    def insertArray(self):
+        pass
+
+    def overwriteChar(self):
+        pass
+
+    def overwriteArray(self):
+        pass
